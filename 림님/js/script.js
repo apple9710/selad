2f58d76791db9a73649eb12fd2f995c7e8f9d9d2
apple9710 <90672022+apple9710@users.noreply.github.com>
2022-04-13 14:26:54 +0900

    Add files via upload

---
+++ 림님/js/script.js
@@ -0,0 +1,21 @@
+
+var me = document.getElementsByClassName
+
+$(document).ready(function() {
+    const menu = $('.category .cat')
+    
+    
+    menu.click(function(e){
+
+        
+
+        $(this).toggleClass('on').siblings().removeClass('on');
+    })
+    
+    
+    
+    
+
+
+
+})
